af7dcbc23118b5a61674f5ead505f6ce67a67451
Michelle Steigerwalt <msteigerwalt@gmail.com>
2013-02-07 00:22:29 -0800

    Changed the way the main Brink API presents itself.

--- src/lib/brink.js
+++ src/lib/brink.js
@@ -1,93 +1,40 @@
-var Brink = Brink || {};
+/**
+ * The main Brink.js file doesn't really have a lot of meat in it. It just
+ * runs the initializer for client or server (changing require methods and the
+ * like where appropriate), then binds all the library modules' interfaces to
+ * the Brink.* namespace, which is made global by the initializer.
+ */
+var Brink;
 
-(function env() {
+Brink = (typeof window !== "undefined")
+      ? require('client/init') : require('./server/init');
 
-	var modules = {};
+var events      = Brink.require('events');
+Brink.fireEvent = events.fire
+Brink.addEvent  = events.on;
 
-	if (typeof window !== "undefined") {
+var enqueue     = Brink.require('enqueue');
+Brink.enqueue   = enqueue.enqueue;
+Brink.callback  = enqueue.callback;
 
-		Brink.client = true;
-
-		Brink.path = function(p) {
-			var args = [];
-			for (var i in arguments) args.push(arguments[i]);
-			return args.join('/');
-		};
-
-		Brink._cexp = function(path, mod) {
-			modules[path] = mod;
-		};
-
-		Brink._creq = function(path) {
-			if (modules[path+'.js']) return modules[path+'.js']();
-			else console.error("Can't find module "+path);
-		}
-
-		window.Brink = Brink;
-
-	} else {
-
-		var path = require('path');
-
-		Brink.server = true;
-
-		Brink.path = function() {
-			var args = [__dirname, '..'];
-			for (var i in arguments) args.push(arguments[i]);
-			return path.join.apply(this, args);
-		};
-
-		global.Brink = Brink;
-		
-	}
-
-}());
-
-Brink.require = function(lib) {
-	return require(Brink.path('lib',lib));
-};
-
-var Queue = Brink.require("queue");
-
-Brink.callback = function(e,d) {
-	if (e) 
-	var i, args = [], fn;
-	for (i in arguments) args.push(arguments[i]);
-	fn = args.shift();
-	if (fn.trigger) fn = fn.trigger;
-	if (fn) fn.apply(fn, args);
-};
-
-Brink.enqueue = function(wrap,q) {
-	var q = q || new Queue(), old = {};
-	for (var k in wrap) {
-		(function(k) {
-			old[k]  = wrap[k];
-			wrap[k] = (function() {
-				var a = arguments;
-				q.push(function() { old[k].apply(old, a); });
-			});
-		})(k);
-	} return q;
-}
-
-var router      = new Brink.require('router')(),
-    Collections = Brink.require('collections');
-
+var router      = new Brink.require('router')();
 Brink.route     = router.route;
-Brink.listen    = router.listen;
 
+var Collections = Brink.require('collections');
 Brink.define    = Collections.define;
 Brink.push      = Collections.push;
 Brink.find      = Collections.find;
 Brink.find_one  = Collections.find_one;
 Brink.create    = Collections.create;
 
-if (Brink.server) {
-	var assets = new require('./server/assets')();
-	assets.attach(router);
+Brink.listen = function(port) {
+	if (Brink.server) {
+		var assets = new require('./server/assets')();
+		assets.attach(router);
+	}
+	router.listen(port);
 }
 
-var transport = new Brink.require('transport')(router);
+var transport  = new Brink.require('transport')(router);
 
 module.exports = Brink;
