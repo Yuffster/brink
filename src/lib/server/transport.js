b3794dcb7d563ae81e9674022ff1d28f0a227ed2
Michelle Steigerwalt <msteigerwalt@gmail.com>
2013-02-10 03:05:09 -0800

    Documentation for Transport is completely outdated.

--- src/lib/server/transport.js
+++ src/lib/server/transport.js
@@ -1,20 +1,3 @@
-/**
- * Transports
- * 
- * So, the point of these two classes is to make sure that any object within
- * a document gets automatically updated when it changes, and that the client
- * and the server both have the same version.
- *
- * We want to make sure that this handles the scenario where the connection
- * between client and server is dropped and another server picks up. We can
- * assume the client missed a few message updates in the interim.
- *
- * We also don't want to be pushing massive amounts of data around, so we're
- * keeping track of which objects are being displayed on which clients.
- *
- * TODO: Make this work for distributing across multiple Brink.js instances.
- */
-
 function Transport(router) {
 
 	var io    = require('socket.io'),
